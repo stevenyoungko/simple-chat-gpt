13520df55b68d4a5d74bfebdc1f841d2b12d07dd
Steven Ko <steven.ko@shoplineapp.com>
2024-02-16 11:30:40 +0800

    feat: page add layout header content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Button } from "antd";
+import { Layout } from "antd";
+import { Header, Content } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 import AntConfigProvider from "../context/antContext";
 
 const Home = () => {
   return (
     <AntConfigProvider>
-      <Button type="link">Button</Button>
+      <Layout className="h-screen">
+        <Header className="bg-gray-100 dark:bg-gray-800 block-border border-0 border-b pr-4">
+          Header
+        </Header>
+        <Layout hasSider>
+          <Sider className="w-1/4 hidden md:block bg-gray-100 dark:bg-gray-800 block-border border-0 border-r">
+            Sider
+          </Sider>
+          <Content>Content</Content>
+        </Layout>
+      </Layout>
     </AntConfigProvider>
   );
 };
