a9671adb16f6a0369563db033afdb047e62ca6da
Steven Ko <steven.ko@shoplineapp.com>
2024-02-16 17:34:09 +0800

    feat: add hook useCommon

---
+++ src/hooks/useCommon.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+
+interface CommonType {
+  openDrawer: boolean;
+  openSetting: boolean;
+  toggleDrawler: (bool?: boolean) => void;
+  toggleSetting: (bool?: boolean) => void;
+}
+
+const useCommon = create<CommonType>()((set, get) => ({
+  openDrawer: false,
+  openSetting: false,
+  toggleDrawler: (bool?: boolean) => {
+    const { openDrawer } = get();
+    set({ openDrawer: typeof bool === "boolean" ? bool : !openDrawer });
+  },
+  toggleSetting: (bool?: boolean) => {
+    const { openSetting } = get();
+    set({ openSetting: typeof bool === "boolean" ? bool : !openSetting });
+  },
+}));
+
+export default useCommon;
