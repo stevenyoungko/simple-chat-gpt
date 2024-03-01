b3df4992e1d8a70761e45a1a67058f9a1f61fc6f
Steven Ko <steven.ko@shoplineapp.com>
2024-03-01 22:20:39 +0800

    feat(HomeApp): add Modal

--- src/containers/HomeApp.tsx
+++ src/containers/HomeApp.tsx
@@ -1,15 +1,40 @@
 "use client";
 import React, { PropsWithChildren, useEffect } from "react";
 import useAntConfig from "@/hooks/useAntConfig";
+import useCommon from "@/hooks/useCommon";
+import { Input, Modal } from "antd";
 
 const HomeApp = ({ children }: PropsWithChildren) => {
   const { initialize: initializeTheme } = useAntConfig();
+  const { openApiKeyModal, apiKey, setApiKeyModal, setApiKey } = useCommon();
 
   useEffect(() => {
     initializeTheme();
-  }, [initializeTheme]);
+    setApiKeyModal(true);
+  }, [initializeTheme, setApiKeyModal]);
 
-  return <>{children}</>;
+  return (
+    <>
+      <Modal
+        open={openApiKeyModal}
+        title="Please enter your API Key:"
+        closable={false}
+        onOk={() => setApiKeyModal(false)}
+        footer={(_, { OkBtn }) => <OkBtn />}
+      >
+        <Input
+          value={apiKey}
+          placeholder="your API key won't be saved in anywhere"
+          onChange={({ target: { value } }) => setApiKey(value)}
+        />
+        <p className="text-xs">
+          You can add API key later in settings, but you can not send message
+          before entering the api key.
+        </p>
+      </Modal>
+      {children}
+    </>
+  );
 };
 
 export default HomeApp;
--- src/hooks/useCommon.ts
+++ src/hooks/useCommon.ts
@@ -3,13 +3,19 @@ import { create } from "zustand";
 interface CommonType {
   openDrawer: boolean;
   openSetting: boolean;
+  openApiKeyModal: boolean;
+  apiKey?: string;
   toggleDrawer: (bool?: boolean) => void;
   toggleSetting: (bool?: boolean) => void;
+  setApiKeyModal: (bool: boolean) => void;
+  setApiKey: (key: string) => void;
 }
 
 const useCommon = create<CommonType>()((set, get) => ({
   openDrawer: false,
   openSetting: false,
+  openApiKeyModal: false,
+  apiKey: undefined,
   toggleDrawer: (bool?: boolean) => {
     const { openDrawer } = get();
     set({ openDrawer: typeof bool === "boolean" ? bool : !openDrawer });
@@ -18,6 +24,8 @@ const useCommon = create<CommonType>()((set, get) => ({
     const { openSetting } = get();
     set({ openSetting: typeof bool === "boolean" ? bool : !openSetting });
   },
+  setApiKeyModal: (bool: boolean) => set({ openApiKeyModal: bool }),
+  setApiKey: (key: string) => set({ apiKey: key }),
 }));
 
 export default useCommon;
