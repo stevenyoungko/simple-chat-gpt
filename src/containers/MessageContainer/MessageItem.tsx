f82ba2abcf7a9afa52741dc5ec796563c22b5a37
Steven Ko <steven.ko@shoplineapp.com>
2024-03-10 19:03:56 +0800

    feat(MessgaeItem): add CopyButton

--- src/containers/MessageContainer/MessageItem.tsx
+++ src/containers/MessageContainer/MessageItem.tsx
@@ -4,6 +4,8 @@ import { MessageType, RoleType } from '@/types/messages';
 import { Icon } from '@iconify/react/dist/iconify.js';
 
 import Markdown from './Markdown';
+import { Space } from 'antd';
+import CopyButton from './CopyButton';
 
 interface MessageItemProps {
   names: {
@@ -17,23 +19,33 @@ interface MessageItemProps {
 const MessageItem = ({ names, message, onDelete }: MessageItemProps) => {
   const { content, role } = message;
   const isGPT = role === RoleType.GPT;
-  const avatarColor = isGPT ? "bg-sky-500" : "bg-orange-500";
+  const avatarColor = isGPT ? 'bg-sky-500' : 'bg-orange-500';
 
   return (
     <div className="relative mt-4 text-sm md:text-base">
       <div className="absolute left-0 top-0">
         <div
           className={clsx(
-            "flex items-center justify-center w-8 h-8 text-white rounded-full",
+            'flex items-center justify-center w-8 h-8 text-white rounded-full',
             avatarColor
           )}
         >
-          <Icon icon={isGPT ? "mdi:face-agent" : "mdi:account"} width={26} />
+          <Icon icon={isGPT ? 'mdi:face-agent' : 'mdi:account'} width={26} />
         </div>
       </div>
       <div className="pl-10 dark:text-gray-100">
         <h4>{isGPT ? names.assistant : names.user}</h4>
         <Markdown content={content} />
+      </div>
+
+      <div className="absolute right-0 top-0">
+        <Space>
+          {isGPT && (
+            <div className="w-7 h-7 rounded-md inline-flex items-center justify-center bg-gray-400 hover:bg-gray-600 dark:bg-gray-600 dark:hover:bg-gray-500 cursor-pointer">
+              <CopyButton text={content} />
+            </div>
+          )}
+        </Space>
       </div>
     </div>
   );
